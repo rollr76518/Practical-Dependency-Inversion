019ee96ca0f8b4f51558ab3c42c95286a6005c5e
Tsungyu Yu <ytyubox@gmail.com>
2020-07-14 18:16:02 +0800

    rewrite MainThreadDecorator by http://blog.benjamin-encz.de/post/main-queue-vs-main-thread/

--- Practical Dependency Inversion/ItemsUIComposer.swift
+++ Practical Dependency Inversion/ItemsUIComposer.swift
@@ -18,16 +18,24 @@ class ItemsUIComposer {
     }
 }
 
-private class MainThreadDecorator: ItemsViewControllerSpec {
+class MainThreadDecorator: ItemsViewControllerSpec {
     
     private let loader: ItemsViewControllerSpec
+    private static var didSetSpecific = false
+    private static let mainQueueKey = DispatchSpecificKey<Int>()
+    private static let mainQueueValue = 1
     
     init(loader: ItemsViewControllerSpec) {
         self.loader = loader
+        if Self.didSetSpecific {
+            return
+        }
+        DispatchQueue.main.setSpecific(key: Self.mainQueueKey, value: 1)
+        Self.didSetSpecific = true
     }
     
     func items(completion: @escaping (Result<[Item], Error>) -> Void) {
-        guard Thread.isMainThread else {
+        guard (DispatchQueue.getSpecific(key: Self.mainQueueKey) == Self.mainQueueValue) else {
             DispatchQueue.main.async(execute: { self.loader.items(completion: completion)} )
             return
         }
